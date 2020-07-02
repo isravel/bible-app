50c67ceb5c9b335a8fd47243474df4e8edb2fa1c
AaronEbinezer <aaroneorchids@gmail.com>
2020-07-03 00:00:55 +0530

    NavScreen

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -2,26 +2,14 @@ import React from 'react';
 import logo from '../logo.svg';
 import '../App.css';
 import home from './HomeScreen.module.css';
+import NavScreen from './NavScreen';
 
 
 class HomeScreen extends React.Component{
     render(){
         return (
             <div className="App">
-                <header className={home.Home}>
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                    Edit <code>src/App.js</code> and save to reload.
-                </p>
-                <a
-                    className="App-link"
-                    href="https://reactjs.org"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    Bible App
-                </a>
-                </header>
+                
       </div>
         );
     }
---
+++ src/screens/NavScreen.js
@@ -0,0 +1,24 @@
+import React from '.react';
+
+const navScreen = () =>{
+    return(
+        <header className={home.Home}>
+                <img src={logo} className="App-logo" alt="logo" />
+                <p>
+                    Edit <code>src/App.js</code> and save to reload.
+                </p>
+                <a
+                    className="App-link"
+                    href="https://reactjs.org"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    Bible App
+                </a>
+                </header>
+
+    );
+
+}
+
+export default navScreen;
